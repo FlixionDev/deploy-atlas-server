0b6bd866b745d895b27c640fb49f1a624fa6e8fc
Flixion <adityasingh200141@gmail.com>
2022-12-30 13:54:26 +0530

    deployes 1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const connect=require('./connect');
 const router=require('./route');
+require('dotenv').config();
+const server=process.env.PORT || 3200;
+
 const app=express();
 app.use(express.json());
 app.use('/',router)
 connect().then((res,rej)=>{
-    app.listen(5000,()=>{
+    app.listen(server,()=>{
         console.log("Server is listening on http://localhost:5000/")
     })
 }).catch((err)=>{
